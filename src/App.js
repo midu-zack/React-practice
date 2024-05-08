21f30208c1070310202171cf44ac7e833fc31150
midu-zack <midlajmidu0002@gmail.com>
2024-05-08 20:10:14 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 // import logo from './logo.svg';
 import React,{Component} from 'react';
 import './App.css';
- 
+import Todo from './componant/Todo';
 
 class App extends Component{
+  state= {
+    myString:'I miss you',
+    myStringTwo : "i have don't problem"
+  
+   }
   render (){
     return(
       <div className="App">
       <h1>Todo App</h1>
+      <h2>{this.state.myString}</h2>
+      
       <h3>Welcome</h3>
-
-      <button>Click Now</button>
+      <button>Click Now</button> 
+      <Todo/>
     </div>
+    
     )
-  }
-}
+  }}
+
 export default App;
