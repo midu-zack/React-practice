df33955afa7a2b94d9859f3511504fabb1714eb0
midu-zack <midlajmidu0002@gmail.com>
2024-05-23 20:44:21 +0530

    callbackREF

--- src/component/Ref.js
+++ src/component/Ref.js
@@ -3,20 +3,21 @@ import { useState, useEffect, useRef } from "react";
 function Ref() {
   const [inputValue, setInputValue] = useState("");
   const count = useRef(0);
+  console.log(count);
 
   useEffect(() => {
     count.current = count.current + 1;
   });
 
   return (
-    <>
+    <div>
       <input
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
       <h1>Render Count: {count.current}</h1>
-    </>
+    </div>
   );
 }
  
